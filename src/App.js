130aa85b72869b7f46f57d0f444b01f1100b12c4
강은빈 <kub0803@naver.com>
2020-10-11 23:15:04 +0900

    Naver-whale

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import logo from './logo.svg';
+//import Mainimg from './images/main.png';
+import Search from './Component/Search';
 import './App.css';
+import Clock from './Component/Clock';
+import Footer from './Component/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+
+  render()
+    return(
+      <>
+        <div className="main">
+          <Search/>
+          <Clock/>
+        </div>
+        <Footer/>
+      </>
+    );
 }
 
 export default App;
