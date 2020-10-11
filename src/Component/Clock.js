04e7a56f750cb0c9b7cf14b19a0467f0c44a1cc4
강은빈 <kub0803@naver.com>
2020-10-11 23:14:22 +0900

    시간 표시 피드백 수정

--- src/Component/Clock.js
+++ src/Component/Clock.js
@@ -2,48 +2,24 @@ import React, { useEffect, useState } from 'react';
 import './Component.css';
 
 function Clock() {
-    //시간
     const [hours, setHours] = useState(new Date().getHours());
-    useEffect(()=>{
-        setInterval(() => {
-            setHours(("0" + new Date().getHours()).slice(-2)) 
-        },1000);
-    })
-    //분
     const [min, setMin] = useState(new Date().getMinutes());
-    useEffect(()=>{
-        setInterval(() => {
-            setMin(("0" + new Date().getMinutes()).slice(-2)) 
-        },1000);
-    })
-    // 초
     const [seco, setSeco] = useState(new Date().getSeconds());
-    useEffect(()=>{
+    const [year, setYear] = useState(new Date().getFullYear());
+    const [month, setMonth] = useState(new Date().getMonth());
+    const [day, setDay] = useState(new Date().getDay());
+
+    useEffect(() => {
         setInterval(() => {
             setSeco(("0" + new Date().getSeconds()).slice(-2)) 
-        },1000);
-    })
-    //년
-    const [year, setYear] = useState(new Date().getFullYear());
-    useEffect(()=>{
-        setInterval(() => {
+            setMin(("0" + new Date().getMinutes()).slice(-2)) 
+            setHours(("0" + new Date().getHours()).slice(-2)) 
             setYear(new Date().getFullYear()) 
-        },1000);
-    })
-    //월
-    const [month, setMonth] = useState(new Date().getMonth());
-    useEffect(()=>{
-        setInterval(() => {
             setMonth(new Date().getMonth()) 
-        },1000);
-    })
-    //일
-    const [day, setDay] = useState(new Date().getDay());
-    useEffect(()=>{
-        setInterval(() => {
             setDay(new Date().getDay()) 
         },1000);
-    })
+    }, []);
+
      return(
          <div className="Date">
             <div className="Clock">
