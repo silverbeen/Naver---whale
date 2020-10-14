96ab41d65e1188329f47e4a3e86f1189595fe290
강은빈 <kub0803@naver.com>
2020-10-14 23:10:39 +0900

    correct

--- src/Component/Clock.js
+++ src/Component/Clock.js
@@ -2,23 +2,24 @@ import React, { useEffect, useState } from 'react';
 import './Component.css';
 
 function Clock() {
-    const [hours, setHours] = useState(new Date().getHours());
-    const [min, setMin] = useState(new Date().getMinutes());
-    const [seco, setSeco] = useState(new Date().getSeconds());
-    const [year, setYear] = useState(new Date().getFullYear());
-    const [month, setMonth] = useState(new Date().getMonth());
-    const [day, setDay] = useState(new Date().getDay());
+    const localDate= new Date();
+    const [hours, setHours] = useState(localDate.getHours());
+    const [min, setMin] = useState(localDate.getMinutes());
+    const [sec, setSec] = useState(localDate.getSeconds());
+    const [year, setYear] = useState(localDate.getFullYear());
+    const [month, setMonth] = useState(localDate.getMonth());
+    const [date, setDate] = useState(localDate.getDate());
 
     useEffect(() => {
         setInterval(() => {
-            setSeco(("0" + new Date().getSeconds()).slice(-2)) 
-            setMin(("0" + new Date().getMinutes()).slice(-2)) 
-            setHours(("0" + new Date().getHours()).slice(-2)) 
-            setYear(new Date().getFullYear()) 
-            setMonth(new Date().getMonth()) 
-            setDay(new Date().getDay()) 
-        },1000);
-    }, []);
+            setSec(("0" + localDate.getSeconds()).slice(-2)) 
+            setMin(("0" + localDate.getMinutes()).slice(-2)) 
+            setHours(("0" + localDate.getHours()).slice(-2)) 
+            setYear(localDate.getFullYear()) 
+            setMonth(localDate.getMonth()+1) 
+            setDate(localDate.getDate()) 
+        }, 1000);
+    });
 
      return(
          <div className="Date">
@@ -28,11 +29,11 @@ function Clock() {
                 </div>
                 <div className="ClockSide">
                     <span>PM</span>
-                    <span>{seco}</span>
+                    <span>{sec}</span>
                 </div>
             </div>
             <div className="dateseo">
-                <span>{year}년 {month}월 {day}일</span>
+                <span>{year}년 {month}월 {date}일</span>
             </div>
         </div>
             
--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,22 +1,28 @@
 import React from "react";
 import './Component.css';
+import FooterPopup from './FooterPopup';
 
 function Footer() {
     
 
-
     return(
-        <div className="footer">
-            <div className="footer-left">
-                <button>스마트웨일</button>
+        <footer>
+            {/* 팝업 */}
+            <FooterPopup/>
+            {/* 이동 */}
+            <div className="footer-bottom">
+                <div className="footer-left">
+                    <button>스마트웨일</button>
+                </div>
+                {/* 왼쪽 하단  */}
+                <div className="footer-rigth">
+                    <span >신성동</span>
+                    <span>할 일 목록</span>
+                    <span>즐겨찾기</span>
+                    <span>강은빈</span>
+                </div>
             </div>
-            <div className="footer-rigth">
-                <span >신성동</span>
-                <span>할 일 목록</span>
-                <span>즐겨찾기</span>
-                <span>강은빈</span>
-            </div>
-        </div>
+        </footer>
     )
 }
 
