381c4bd3d3ee0435853105a69ace9f2a792b2d9e
강은빈 <kub0803@naver.com>
2020-10-15 12:09:39 +0900

    Footer

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import './Component.css';
-import FooterPopup from './FooterPopup';
+import PopupWeather from './FooterCom/PopupWeather';
+import PopupTodo from './FooterCom/PopupTodo';
 
 function Footer() {
     
+    const [weather,setWeather]=useState(null);
+    const [todo, setTodo]=useState(null);
 
     return(
         <footer>
-            {/* 팝업 */}
-            <FooterPopup/>
+            {/*날씨 */}
+            <PopupWeather weather={weather}/>
+            {/* 할 일 목록 */}
+            <PopupTodo todo={todo}/>
+
             {/* 이동 */}
             <div className="footer-bottom">
                 <div className="footer-left">
@@ -16,8 +22,14 @@ function Footer() {
                 </div>
                 {/* 왼쪽 하단  */}
                 <div className="footer-rigth">
-                    <span >신성동</span>
-                    <span>할 일 목록</span>
+                    <span 
+                        onMouseOver={()=>{setWeather("block")}}
+                        onMouseLeave={()=>{setWeather("none")}}
+                    >신성동</span>
+                    <span 
+                        onClick={()=>{setTodo("flex")}}
+                        onDoubleClick={()=>{setTodo("none")}}
+                    >할 일 목록</span>
                     <span>즐겨찾기</span>
                     <span>강은빈</span>
                 </div>
